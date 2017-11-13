1d4fd8d96d0ea000b951478a3551a0f74b5bc3fd
neilmarshall <neil.marshall@dunelm.org.uk>
2017-11-13 23:41:51 +0000

    add example using 'parser.set_defaults'

--- clp_ex_1.py
+++ clp_ex_1.py
@@ -79,6 +79,36 @@ def clp_3(*args):
         return [args.pos1, args.pos2, args.pos3]
 
 
+def clp_4(*args):
+    """
+    As for example 3, but the override flag takes no arguments.
+
+    This example shows how to use parser.set_defaults.
+    """
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='cmd')
+
+    # create the parser for the override that sums a list of integers
+    parser_func = subparsers.add_parser('func')
+    parser_func.set_defaults(func=sum)
+
+    # create the parser for the situation where the override is not called and returns the integers passed
+    parser_store = subparsers.add_parser('store')
+    parser_store.add_argument('pos1', type=int)
+    parser_store.add_argument('pos2', type=int)
+    parser_store.add_argument('pos3', type=int, nargs='?')
+
+    args = parser.parse_args(*args)
+
+    ints = [4, 5, 6]
+
+    if args.cmd == 'func':
+        return args.func(ints)
+    else:
+        return [args.pos1, args.pos2, args.pos3]
+
+
 class Test_clp(unittest.TestCase):
 
     def test_args_ex_1_1(self):
@@ -104,6 +134,15 @@ class Test_clp(unittest.TestCase):
 
     def test_args_ex_3_4(self):
         self.assertEqual(clp_3(['store', '6', '7', '8']), [6, 7, 8])
+
+    def test_args_ex_4_1(self):
+        self.assertEqual(clp_4(['func']), 15)
+
+    def test_args_ex_4_2(self):
+        self.assertEqual(clp_4(['store', '6', '7']), [6, 7, None])
+
+    def test_args_ex_4_3(self):
+        self.assertEqual(clp_4(['store', '6', '7', '8']), [6, 7, 8])
 
 
 if __name__ == "__main__":
